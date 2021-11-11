6a2541a23fe2bfcdf4d8f0498762ad73e0a67794
MishinK <mka456@yandex.ru>
2021-11-11 10:59:14 +0500

    d04

---
+++ d04/Assets/Scripts/myScripts/ex02/Trap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    private Sonic sonic;
+    private bool activated;
+    private Vector3 initialPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sonic = GameObject.Find("Sonic").GetComponent<Sonic>();
+        initialPos = transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated && transform.position.y <= 17.82)
+        {
+            transform.Translate(Vector3.up * 3.0f * Time.deltaTime);
+        }
+        if (transform.position.y >= 17.82 && activated)
+        {
+           transform.Translate(Vector3.up * -3.0f * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!activated)
+        {
+            activated = true;
+        }
+    }
+}
